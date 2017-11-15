e93c252353e432e50c03e31e54cce5271902c418
Ondrej Hlavacek <ondrej.hlavacek@keboola.com>
2017-11-15 16:57:39 +0100

    fix(tests): method name case

--- tests/run.php
+++ tests/run.php
@@ -6,11 +6,11 @@ $testFolder = __DIR__;
 $finder = new \Symfony\Component\Finder\Finder();
 $finder->directories()->sortByName()->in($testFolder)->depth(0);
 foreach ($finder as $testSuite) {
-    print "Test " . $testSuite->getPathName() . "\n";
+    print "Test " . $testSuite->getPathname() . "\n";
     $temp = new \Keboola\Temp\Temp("processor-decompress");
     $temp->initRunFolder();
 
-    $copyCommand = "cp -R " . $testSuite->getPathName() . "/source/data/* " . $temp->getTmpFolder();
+    $copyCommand = "cp -R " . $testSuite->getPathname() . "/source/data/* " . $temp->getTmpFolder();
     (new \Symfony\Component\Process\Process($copyCommand))->mustRun();
 
     mkdir($temp->getTmpFolder() . "/out/tables", 0777, true);
@@ -19,10 +19,8 @@ foreach ($finder as $testSuite) {
     $runCommand = "php /code/main.php --data=" . $temp->getTmpFolder();
     $runProcess = new \Symfony\Component\Process\Process($runCommand);
     $runProcess->mustRun();
-
-    var_dump($runProcess->getOutput());
-
-    $diffCommand = "diff --exclude=.gitkeep --ignore-all-space --recursive " . $testSuite->getPathName() . "/expected/data/out " . $temp->getTmpFolder() . "/out";
+    
+    $diffCommand = "diff --exclude=.gitkeep --ignore-all-space --recursive " . $testSuite->getPathname() . "/expected/data/out " . $temp->getTmpFolder() . "/out";
     $diffProcess = new \Symfony\Component\Process\Process($diffCommand);
     $diffProcess->run();
     if ($diffProcess->getExitCode() > 0) {
